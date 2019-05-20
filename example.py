84df945458c8cd54499bdef87afcb3c5f21d1f7b
Violeta Gonzalez-Perez <violegp@gmail.com>
2019-05-20 15:22:14 +0100

    Added an example script that deals with the example_data.csv file

---
+++ example.py
@@ -0,0 +1,6 @@
+import pycode.novonix_add as prep
+
+infile = 'example_data/example_data.csv'
+
+prep.prepare_novonix(infile, addstate=True, lprotocol=True,
+                overwrite=False, verbose= True)
